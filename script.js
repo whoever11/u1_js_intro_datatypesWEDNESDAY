1669ba87922852756e1afa1a4a61c5ca1efb0a53
whoever11 <cjthesedays000@gmail.com>
2024-01-17 12:27:48 -0500

    updated with array

--- script.js
+++ script.js
@@ -34,8 +34,8 @@
 
 //console.log( "ten" + 10)
 
-let isTodayWednesday = true //boolean example
-let isTodayThursday = false //boolean example
+//let isTodayWednesday = true //boolean example
+//let isTodayThursday = false //boolean example
 
 //Logical Operators Below!
 //&& - and
@@ -44,3 +44,36 @@ let isTodayThursday = false //boolean example
 // if something doesn't exist it is either null or undefined 
 //arrays are a list of items
 //objects are a collection of key value pairs
+
+//ARRAYS SECOND LESSON OF WEDNESDAY CLASS JANUARY 17,2024
+// arrays let you collect, store data, reorder, and access data
+
+//let friends = [`Moe`, `Larry`, `Curly`] //array list practice
+//console.log(friends.length) will show all the friends
+//console.log(friends[2]) // will show curly because you count from 0
+//console.log(friends[3]) // will show undefined because there is no 3rd friend
+//let firstFriend = friends[0]
+//console.log(firstFriend.length)
+//below is array methods fruits
+let fruits = [`apple`, `banana`, `orange`, `strawberry`, `kiwi`, `grapes`,
+`mango`, `pineapple`, `pear`] //array list practice
+//console.log(fruits[fruits.length - 1]) //will show all the fruits in the array at the end fruit
+//console.log(fruits.toString())//will show all the fruits in the array as a string(line in order in terminal)
+//fruits.pop()//.pop()will remove the last fruit in the array
+//fruits.push('peach')//.push() will add peach to the end of the array
+
+//fruits.shift()//.shift() will remove the first fruit in the array and shift everything to the left
+
+//fruits.unshift('blueberry')//.unshift() will add blueberry to the beginning of the array and shift everything to the right
+
+//fruits.reverse()//.reverse() will reverse the order of the array
+
+//.splice is very powerful
+//it can add, it can remove, it can edit
+//from any part of the array
+//it will take 3 arguments
+//where we started, where we stop, and what we're doing
+
+
+fruits.splice(0,2,'dragonfruit', 'avacado' )//.splice() will remove orange and strawberry and add dragonfruit and avacado
+console.log(fruits)
