45558342061ab5c7936b5cee6f2724b71375d79d
whoever11 <cjthesedays000@gmail.com>
2024-01-17 11:36:10 -0500

    javascript intro

---
+++ script.js
@@ -0,0 +1,46 @@
+//Intro to Javascript
+//Working with the Number datatypes
+// + - * / % **
+// we also have % called a modulo operator
+
+// this is called string concatenation
+
+//console.log("hello world")
+
+
+//EIC=error in our code
+//EIC=error in chair
+//PebcaK=problem exists between chair and keyboard
+//id10t= id10t error
+//backtick is the key above the tab key VERY IMPORTANT FOR FUTURE CODING LINES LIKE LINE 33
+//console.log('hello world'.length)
+
+//defining Variables
+//let, const, var
+//let is a variable that can change
+//const is a variable that cannot change
+//var is a variable that can change
+// let and const are main two, var can be seen as outdated 
+
+//let age = 25
+//let monthsInYear = 12
+//let ageInMonths = age * monthsInYear
+//let firstName = "Cj"
+//syntactic sugar
+//console.log(`My name is ${firstName} and I am ${age} years old.`)
+//console.log(`hello my name is ${firstName} and I am ${ageInMonths} months old`)
+//Literal
+//console.log(`my name is ${firstName} and I am ${age} years old, next year I will be ${age+=1} years old`)
+
+//console.log( "ten" + 10)
+
+let isTodayWednesday = true //boolean example
+let isTodayThursday = false //boolean example
+
+//Logical Operators Below!
+//&& - and
+//|| - or
+// ! - not
+// if something doesn't exist it is either null or undefined 
+//arrays are a list of items
+//objects are a collection of key value pairs
